1a614abe76137f49052fa0cf7d70c39b955fc2a9
Tiago-salva <maitetiago58@gmail.com>
2023-04-23 22:51:57 -0300

    Proyect finished (With UI)

--- script.js
+++ script.js
@@ -1,53 +1,105 @@
-const choices = ["Piedra", "Papel", "Tijeras"];
+const choices = ["Rock", "Paper", "Scissors"];
 let playerScore = 0;
 let computerScore = 0;
 
 function getComputerChoice() {
     let result = choices[Math.floor(Math.random() * choices.length)];
     return result;
-} 
+}; 
 
-let playerChoice = ""
-let computerChoice = ""
+const btnRock = document.querySelector("#buttonR");
+const btnPaper = document.querySelector("#buttonP");
+const btnScissors = document.querySelector("#buttonS");
+
+const pPlayerScore = document.querySelector(".player-Score");
+const pComputerScore = document.querySelector(".computer-Score");
+
+const pResult = document.querySelector(".result");
+
+const pResultAgain = document.querySelector(".result-Again");
+
+const btnAgain = document.getElementById("btn-Again");
+
+pPlayerScore.textContent = (`Player : ${playerScore}`);
+pComputerScore.textContent = (`Computer : ${computerScore}`);
+pResult.innerHTML = ("¿Are you ready? <br> Choose Rock, Paper or Scissors!");
 
 function playRound(playerSelection, computerSelection) {
-    
-    console.log(`Tu elegiste: ${playerChoice}`)
-    console.log(`La computadora eligio: ${computerChoice}`)
-    
+    if (playerScore === 5 || computerScore === 5) {
+        return; // Sale del juego si alguien ha ganado
+    }
+
     if (playerSelection === computerSelection) {
         playerScore += 1;
         computerScore += 1;
-        console.log("Es un empate!");
+        pResult.textContent = ("Wow, it's a tie!");
     } else if (
-        (playerSelection === "Piedra" && computerSelection === "Tijeras") || 
-        (playerSelection === "Papel" && computerSelection === "Piedra") || 
-        (playerSelection === "Tijeras" && computerSelection === "Papel")) {
+        (playerSelection === "Rock" && computerSelection === "Scissors") || 
+        (playerSelection === "Paper" && computerSelection === "Rock") || 
+        (playerSelection === "Scissors" && computerSelection === "Paper")) {
             playerScore += 1;
-            console.log(`Ganaste! ${playerSelection} le gana a ${computerSelection}`)
+            pResult.textContent = (`You win! ${playerSelection} beats ${computerSelection}`)
     } else {
         computerScore += 1;
-        console.log(`Perdiste! ${computerSelection} le gana a ${playerSelection}`)
+        pResult.textContent = (`Oh no! ${playerSelection} can't beat ${computerSelection}`)
     }
-    console.log(`Player score: ${playerScore}\nComputer score: ${computerScore}`)
-    console.log("---------------------------------------------")
-}
-
-while (playerScore != 5 && computerScore != 5) {
-    playerChoice = prompt("Elige Piedra, Papel o Tijeras!").toLowerCase() 
-    playerChoice = playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1);
-    computerChoice = getComputerChoice()
-    playRound(playerChoice, computerChoice)
+    pPlayerScore.textContent = (`Player: ${playerScore}`);
+    pComputerScore.textContent = (`Computer: ${computerScore}`);
+    Winners();
 }
 
 function Winners() {
     if (playerScore === 5) {
-        console.log("Le haz ganado a la maquina!")
+        showDialog();
+        pResultAgain.textContent = ("Amazing, You beat the computer!");
     } else if (computerScore === 5) {
-        console.log("La computadora ha ganado ]: ")
+        showDialog();
+        pResultAgain.textContent = ("I'm sorry, You lost against the computer");
     } else if (playerScore === 5 && computerScore === 5) {
-        console.log("Es un empate!")
+        showDialog();
+        pResultAgain.textContent = ("Uf, It's a tie!");
     }
 }
 
-Winners()
+function showDialog() {
+    document.getElementById("again").style.display = "block";
+}
+
+function closeDialog() {
+    document.getElementById("again").style.display = "none";
+}
+
+btnRock.addEventListener("click", () => {
+    if (playerScore === 5 || computerScore === 5) {
+        return;
+    }       
+    let playerSelection = "Rock";
+    let computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
+});
+    
+btnPaper.addEventListener("click", () => {
+    if (playerScore === 5 || computerScore === 5) {
+        return;
+    }
+    let playerSelection = "Paper";
+    let computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
+});
+    
+btnScissors.addEventListener("click", () => {
+    if (playerScore === 5 || computerScore === 5) {
+        return;
+    }
+    let playerSelection = "Scissors";
+    let computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
+});
+
+btnAgain.addEventListener("click", () => {
+    playerScore = 0;
+    computerScore = 0;
+    pPlayerScore.textContent = (`Player: ${playerScore}`);
+    pComputerScore.textContent = (`Computer: ${computerScore}`);
+    closeDialog();
+});
